b299318e3c5b1d6ff454f26c253647f5b88ccd30
shohel-ahmed <shohelahmed27.sa@gmail.com>
2025-02-10 23:35:52 +0800

    changes code

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var num=Number(prompt("Enter a number : "));
 var result =num>0 ? "positive" : "negative"
 document.write(result);
 
-document.write ("End here");
+document.write ("End here <br>");
 
 var number = Number(prompt("enter a number again"));
-var res=number>0 ? "positive" : number<0 ?"negetive" : "zero";
+var res=number>0 ? "positive" : number<0 ? "negative" : "zero";
+document.write(res);
