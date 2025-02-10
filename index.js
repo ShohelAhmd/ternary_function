7492ee8478eb07d4c16f9244d45ba2b64400c79d
shohel-ahmed <shohelahmed27.sa@gmail.com>
2025-02-10 23:32:42 +0800

    added numbering

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var num=Number(prompt("Enter a number : "));
 var result =num>0 ? "positive" : "negative"
 document.write(result);
+
+document.write ("End here");
+
+var number = Number(prompt("enter a number again"));
+var res=number>0 ? "positive" : number<0 ?"negetive" : "zero";
